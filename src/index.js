e8a24b682a340d1cc1f8cd3513d49b00ea6d9135
Md-Modassar <mdmodassar622@gmail.com>
2022-12-19 15:38:35 +0530

    first push

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express=require('express')
+const mongoose=require('mongoose')
+const route=require('./route/route')
+const app=express()
+const multer=require('multer')
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(multer().any())
+
+mongoose.connect('mongodb+srv://modassar123:modassar1234@test.ahxnnau.mongodb.net/group02Database',{
+    useNewUrlParser:true
+},mongoose.set('strictQuery', false))
+.then(()=>console.log("mongoose is connected"))
+.catch(err=>console.log(err))
+
+app.use('/',route)
+
+app.listen(3000,()=>{
+    console.log('express app running on port 3000')
+})
