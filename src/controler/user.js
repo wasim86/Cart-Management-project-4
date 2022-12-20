0fb9a8d2002605470a253afeae059c824bea979f
Md-Modassar <mdmodassar622@gmail.com>
2022-12-20 11:47:29 +0530

    done

--- src/controler/user.js
+++ src/controler/user.js
@@ -81,3 +81,87 @@ exports.create=async function(req,res){
     }
 }
 
+
+exports.getuser=async function(req,res){
+  try{const userid=peq.params.userId
+
+  if(!objectId.isValid(userid)){return res.status(400).send({status:false,msg:"please enter valide id"})}
+  const data=await usermodel.findById(userid)
+  if(!data){return res.status(400).send({status:false,msg:"userid is not valid"})}
+  res.status(200).send({status:true,msg:'User profile details',data:data})
+  }catch(err){return res.status(500).send({status:false,msg:err.message})}
+
+  
+}
+
+exports.updateuser=async function(req,res){
+  try{const userid=req.params.userId
+  if(!objectId.isValid(userid)){return res.status(400).send({status:false,msg:"please enter valide userid"})}
+  const data=req.files
+  const {fname,lname,email,phone,password}=data
+  if(fname){
+    if(!isValide(fname)){return res.status(400).send({status:false,msg:"Please enter valide fname"})}
+    data.fname=fname
+  }
+  if(lname){
+    if(!isValide(lname)){return res.status(400).send({status:false,msg:"Please enter valide lname"})}
+    data.lname=lname
+  }
+  if(email){
+    if(!isValidEmail(email)){return res.status(400).send({status:false,msg:"Please enter valide email"})}
+    data.email=email
+  }
+  if(phone){
+    if(!isValideMobile(phone)){return res.status(400).send({status:false,msg:"Please enter valide phone"})}
+    data.phone=phone
+  }
+  if(password){
+    if(!isValidPassword(password)){return res.status(400).send({status:false,msg:"Please enter valide password"})}
+    const salt = await bcrypt.genSalt(10)
+    const secPass =await bcrypt.hash(password,salt)
+    data.password=secPass
+  }
+  if(files.address.shipping.street){
+    let street=files.billing.address.street
+    if(!isValide(street)){return res.status(400).send({status:false,msg:"Please enter valide street"})}
+     data.address.shipping.street=street
+  }
+  if(files.address.address.city){
+    let city=files.billing.address.city
+    if(!isValide(city)){return res.status(400).send({status:false,msg:"Please enter valide city"})}
+    data.billing.address.city=city
+  }
+  if(files.address.address.pincode){
+    let pincode=files.billing.address.pincode
+    if(!isValide(pincode)){return res.status(400).send({status:false,msg:"Please enter valide pincode"})}
+    data.billing.address.pincode=pincode
+  }
+
+  if(files.billing.shipping.street){
+    let street=files.billing.address.street
+    if(!isValide(street)){return res.status(400).send({status:false,msg:"Please enter valide street"})}
+    data.billing.address.street=street
+  }
+  if(files.billing.address.city){
+    let city=files.billing.address.city
+    if(!isValide(city)){return res.status(400).send({status:false,msg:"Please enter valide city"})}
+    data.billing.address.city=city
+  }
+  if(files.billing.address.pincode){
+    let pincode=files.billing.address.pincode
+    if(!isValide(pincode)){return res.status(400).send({status:false,msg:"Please enter valide pincode"})}
+    data.billing.address.pincode=pincode
+  }
+
+
+  const emailvalid=await usermodel.findOne({email:email})
+  if(emailvalid){return res.status(409).send({status:false,msg:"email is already existing"})}
+  const phonevalid=await usermodel.findOne({email:email})
+  if(phonevalid){return res.status(409).send({status:false,msg:"phone is already existing"})}
+ 
+  const updatedata=await usermodel.findByIdAndUpdate(userid,{$set:data},{new:true})
+  if(!updatedata){return res.status(404).send({status:false,msg:"userid is not exist"})}
+  return res.status(200).send({status:true,msg:"User profile updated",data:updatedata})
+}catch(err){return res.status(500).send({status:false,msg:err.message})}
+}
+
