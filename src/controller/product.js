9a7d7b388e1244aba9f99ccd477cf81cef8f392d
Md-Modassar <mdmodassar622@gmail.com>
2022-12-22 13:49:33 +0530

    done FEATTURE II

--- src/controller/product.js
+++ src/controller/product.js
@@ -1,7 +1,8 @@
 const aws= require("aws-sdk")
 const{uploadFile}=require("../aws/aws")
 const productmodel=require('../models/productModel')
-
+const mongoose=require('mongoose')
+const objectId=mongoose.Types.ObjectId
 
 function isValide(value) {
     return (typeof value === "string" && value.trim().length > 0 && value.match(/^[A-Za-z ][A-Za-z _]{1,100}$/));
@@ -9,41 +10,53 @@ function isValide(value) {
 //  function isValidprice(value){
 //     return (typeof value === "number" &&  value.trim().length > 0 && value.match(/^[0-9]{10}$/))
 //   }
+
 exports.createproduct=async function(req,res){
-    try{
-        const files=req.files
-        if(files && files.length==0) return res.status(400).send({status:false,msg:"files are empty"})
-        let uploadedFileURL= await uploadFile( files[0] )
-        const data=req.body
-        const {title,description,price,currencyId,currencyFormat, availableSize}=data
-        data.productImage=uploadedFileURL
+  try{
+      const files=req.files
+      if(files && files.length==0) return res.status(400).send({status:false,msg:"files are empty"})
+      let uploadedFileURL= await uploadFile( files[0] )
+      const data=req.body
+      const {title,description,price,currencyId,currencyFormat,availableSizes}=data
+      
+      data.productImage=uploadedFileURL
 
-    
-    if(!title){return res.status(400).send({status:false,msg:"Please provide title"})}
-    if(!description){return res.status(400).send({status:false,msg:"Please provide description"})}
-    if(!price){return res.status(400).send({status:false,msg:"Please provide price"})}
-    if(!currencyId){return res.status(400).send({status:false,msg:"Please provide currencyId"})}
-    if(!currencyFormat){return res.status(400).send({status:false,msg:"Please provide currencyformate"})}
-    if(!isValide(title)){return res.status(400).send({status:false,msg:'please enter valide titel'})}
-    if(!isValide(description)){return res.status(400).send({status:false,msg:'please enter valide description'})}
-    if(!isValide(currencyId)){return res.status(400).send({status:false,msg:'please enter valide currencyId'})}
-     if(price)data.price=price+'₹'
-    const arr=["S", "XS","M","X", "L","XXL", "XL"]
-    if(availableSize){
-        if(!arr.includes(availableSize)){return res.status(400).send({status:false,msg:"please enter size it should be like these ['S', 'XS','M,'X','L','XXL','XL']"})}
-    }
-    const arr1=['INR']
-    if(!arr1.includes(currencyId)){return res.status(400).send({status:false,msg:'please enter only INR'})}
-    if(currencyFormat!='₹'){return res.status(400).send({status:false,msg:"enter valide sybol"})}
+   
+  if(!title){return res.status(400).send({status:false,msg:"Please provide title"})}
+  if(!description){return res.status(400).send({status:false,msg:"Please provide description"})}
+  if(!price){return res.status(400).send({status:false,msg:"Please provide price"})}
+  if(!currencyId){return res.status(400).send({status:false,msg:"Please provide currencyId"})}
+  if(!currencyFormat){return res.status(400).send({status:false,msg:"Please provide currencyformate"})}
+  if(!isValide(title)){return res.status(400).send({status:false,msg:'please enter valide titel'})}
+  if(!isValide(description)){return res.status(400).send({status:false,msg:'please enter valide description'})}
+  if(!isValide(currencyId)){return res.status(400).send({status:false,msg:'please enter valide currencyId'})}
+   if(price)data.price=price
+  const arr=["S", "XS","M","X", "L","XXL", "XL"]
+  
+    const arrsize= availableSizes.split(",").map(x=>x.trim())
+  if(availableSizes){
+      for(let i=0; i<arrsize.length; i++){ 
+       
+      if(!arr.includes(arrsize[i])){
+      
+        return res.status(400).send({status:false,msg:"please enter size it should be like these ['S', 'XS','M,'X','L','XXL','XL']"})}
+        data.availableSizes=arrsize
+      }
+     
+  }
+  const arr1=['INR']
+  if(!arr1.includes(currencyId)){return res.status(400).send({status:false,msg:'please enter only INR'})}
+  if(currencyFormat!='₹'){return res.status(400).send({status:false,msg:"enter valide symbol"})}
 
-    const titilexist=await productmodel.findOne({title:title})
-    if(titilexist){return res.status(400).send({status:false,msg:'title is already exist'})}
+  const titilexist=await productmodel.findOne({title:title})
+  if(titilexist){return res.status(400).send({status:false,msg:'title is already exist'})}
 
-    const createdata=await productmodel.create(data)
-    return res.status(201).send({status:true,msg:"successfully",data:createdata})
-    }catch(err){return res.status(500).send({status:false,msg:err.message})}
+  const createdata=await productmodel.create(data)
+  return res.status(201).send({status:true,msg:"successfully",data:createdata})
+  }catch(err){return res.status(500).send({status:false,msg:err.message})}
 
 }
+
 
 
 exports.getproduct = async function (req, res) {
@@ -54,18 +67,20 @@ exports.getproduct = async function (req, res) {
     if(data.size) newdata.availableSizes=data.size
     if(data.name) newdata.title=data.name 
     if(data.price)newdata.price=data.price
+    let new2data={}
+    if(data.priceshort==-1) new2data.price=-1
+     new2data.price=1
      let abc={}
     if(data.priceGreaterThan||data.priceLessThan){
       if(data.priceGreaterThan) abc.$gt=data.priceGreaterThan
       if(data.priceLessThan) abc.$lt=data.priceLessThan
-        //newdata.price=abc
-      let pricedata=await productmodel.find({price:abc,isDeleted:false})
-      return res.status(200).send({status:true,data:pricedata})
+      console.log(abc)
+        newdata.price=abc
+      // let pricedata=await productmodel.find({price:abc,isDeleted:false}).sort(new2data)
+      // return res.status(200).send({status:true,data:pricedata})
 
     }
-    let new2data={}
-    if(data.priceshort==-1) new2data.price=-1
-     new2data.price=1
+   
     let finddata= await productmodel.find(newdata).sort(new2data)
     if(finddata.length==0) return res.status(404).send({status:false,msg:"data is not find"})
     return res.status(200).send({status:true,data:finddata})
@@ -154,9 +169,9 @@ exports.updateProduct = async function(req,res){
 
 exports.deleteProductById= async (req,res) => {
     try{
-        const productId = req.params.productsById
+        const productId = req.params.productId
 
-        if(!objectId.isValid(productId)){return res.status(400).send({ status: false, message:"please enter valid productId in params",})}
+        if(!objectId.isValid(productId)){return res.status(400).send({ status: false, message:"please enter valid productId in params"})}
 
         const deletedProduct = await productmodel.findOneAndUpdate(
            { _id:productId,isDeleted:false },{ $set: { isDeleted: true, deletedAt: new Date()}}, { new: true } );
--- src/models/productModel.js
+++ src/models/productModel.js
@@ -3,13 +3,13 @@ const mongoose=require('mongoose')
 const productSchema=new mongoose.Schema({
     title: {type:String, required:true, unique:true,trim: true},
      description: {type:String,required:true,trim: true},
-     price: {type:String, required:true,trim: true},
+     price: {type:Number, required:true,trim: true},
      currencyId: {type:String, required:true,trim: true,default:'INR'},//INR
      currencyFormat: {type:String,required:true,Symbol:'₹'},
      isFreeShipping: {type:Boolean, default: false},
      productImage: {type:String, required:true,trim: true},  // s3 link
      style: {type:String,trim: true},
-     availableSizes: {type:[String],default:['XS'], enum:["S", "XS","M","X", "L","XXL", "XL"]},
+     availableSizes: {type:[String],default:['XS']},
      installments: {type:Number},
      deletedAt: {type:Date}, 
      isDeleted: {type:Boolean, default: false}
